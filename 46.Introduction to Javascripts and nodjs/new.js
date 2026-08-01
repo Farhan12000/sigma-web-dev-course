9fb822937321b6ef3c26753730e833a2a072e73c
Farhan <281381464+Farhan12000@users.noreply.github.com>
2026-08-02 00:13:13 +0530

    JavaScipt
    So happy to start my interactive journey with javascripts and Nodejs

---
+++ 46.Introduction to Javascripts and nodjs/new.js
@@ -0,0 +1,7 @@
+
+console.log("Code is running...")
+console.log("Hello World")
+console.log("Code is also running...")
+console.log("Code is looking like wow...")
+
+
---
+++ 46.Introduction to Javascripts and nodjs/script.js
@@ -0,0 +1,23 @@
+alert("hello World")
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like wow...")
+
+
+var a = prompt("Enter your Number...")
+var isTrue = confirm("Are you sure you want to leave and blast your computer")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+
+console.log("your number is " + a)
+
+document.title = "Hey I am Good"
+
+document.body.style.backgroundColor = "black"
+
