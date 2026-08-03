82b84fff9712fe3d2b6665efbcb15a5105542e04
Farhan <281381464+Farhan12000@users.noreply.github.com>
2026-08-03 23:01:14 +0530

    JavaScripts

---
+++ 47.Variable ,datatypes &operaters-JavaScripts/Scripts.js
@@ -0,0 +1,53 @@
+console.log("Hey this is tutorial 55 of web development")
+
+// var a = 6
+// var b = 5
+// var c ="farhan"
+// var _a = "shadab"
+// //var 55a = "Rohan;" //Not Allowed
+
+
+// console.log(a+b+8)
+// console.log(typeof a, typeof b, typeof c)
+
+//const a1 = 6;
+//a1 =a1 + 1 ;
+//Not Allowed bcz a1 is constant
+
+// let a = 6
+// let b = 5
+// let c ="farhan"
+// let _a = "shadab"
+
+// {
+//     var a = 66;
+//     console.log(a)
+// }
+// console.log(a)
+
+// let x ="farhan bhai" ;
+// let y = 22 ;
+// let z = 3.55;
+// const p = true;
+// let r =null;
+// let q = undefined;
+
+// console.log(x ,y, z, p, r,q)
+// console.log(typeof x ,typeof y,typeof z,typeof p,typeof r, typeof q)
+
+
+let o = {
+    name: "farhan bhai",
+    "job Code": 7869 ,
+    "is_handsome" : true 
+
+}
+console.log(o)
+
+o.sarlary ="100cr"
+
+console.log(o)
+
+o.sarlary ="500cr"
+
+console.log(o)
