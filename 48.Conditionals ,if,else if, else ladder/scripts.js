0e4ca4c56bcf606c281642b1e7556ee430528f82
Farhan <281381464+Farhan12000@users.noreply.github.com>
2026-08-03 23:47:39 +0530

    JavaScript

---
+++ 48.Conditionals ,if,else if, else ladder/scripts.js
@@ -0,0 +1,39 @@
+console.log("I am conditional tutorial")
+
+let age =1;
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+
+if (age == 18){
+    console.log("You are eligible to drive");
+}
+else if (age==0){
+    console.log("Are You Kidding Me")
+}
+else if (age==1){
+    console.log("Are You again Kidding Me")
+}
+
+
+else{
+    console.log("You are not eligible to drive");
+}
+
+/*
+hey i am
+multiline comment
+*/
+a = 6;
+b = 8;
+let c= a>b ? (a-b) : (b-a) ;
+
+if(a>b){
+    let c = a -b;   
+}
+else{
+    let c = a - b;
+}
