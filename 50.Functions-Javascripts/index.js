21162b0f673c623dea855e67075be1d7bb13a82c
Farhan <281381464+Farhan12000@users.noreply.github.com>
2026-08-04 22:55:55 +0530

    JavaScripts

---
+++ 50.Functions-Javascripts/index.js
@@ -0,0 +1,39 @@
+// console.log("hey this is functions Tuorial")
+// console.log("I am farhan")
+// console.log("I am farhan and I am 25 years old")
+// console.log("I am farhan and I am good boy")
+// console.log("I am farhan and I am from delhi")
+// console.log("I am farhan and I become Web developer")
+
+// function nice(name){
+//     console.log("I am " + name)
+// console.log("I am " + name + " and I am 25 years old")
+// console.log("I am " + name +  " and I am good boy")
+// console.log("I am " + name + " and I am from delhi")
+// console.log("I am " + name + " and I become Web developer")
+// }
+
+// nice("shadab")
+
+// function sum(a,b,c=3){
+//     // console.log( a+b)
+//     return a + b+c;
+// }
+
+
+// result1 = sum(6,7)
+// result2 = sum(9,7)
+// result3 = sum(8,7)
+
+
+// console.log("The sum of these number is:", result1)
+// console.log("The sum of these number is:", result2)
+// console.log("The sum of these number is:", result3)
+
+const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+}
+
+func1(67)
+func1(54)
+func1(90)
