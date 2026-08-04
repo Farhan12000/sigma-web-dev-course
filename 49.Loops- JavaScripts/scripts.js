9958abf80ccb304bf32b2265f4e0072b14fa6914
Farhan <281381464+Farhan12000@users.noreply.github.com>
2026-08-04 22:08:46 +0530

    javaScripts

---
+++ 49.Loops- JavaScripts/scripts.js
@@ -0,0 +1,35 @@
+ console.log("hey this is Loops tutorial")
+
+// let a = 1 ;
+
+// for(let i = 0; i< 100; i++){
+//     console.log( a+i)
+// }
+
+// let obj = {
+//     name: "farhan",
+//     role: "Programmer",
+//     Company: "CodeWithFarhan"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element)    
+// }
+
+// for (const c of "farhan") {
+//  console.log(c)    
+// // }
+
+// let i = 0;
+// while(i<6){
+//     console.log(i);
+//     i++;
+// }
+let i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i<6) ;
+
+
