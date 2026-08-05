0b1d513028acd9cc5e0446c139dbec201419eef2
Farhan <281381464+Farhan12000@users.noreply.github.com>
2026-08-05 23:05:20 +0530

    String-JavaScripts

---
+++ 52.JavaScripts -String/index.js
@@ -0,0 +1,31 @@
+console.log("Hey this is String Tutorial")
+
+let a = ("Farhan")
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+console.log(a.length)
+
+let real_name = "Shadab"
+let friend = "Farhan"
+
+console.log("his name is "+ real_name + "  and his friend name is "+ friend ) 
+console.log(`his name is ${real_name} and his friend name is ${friend}`) //way to same line use ``
+
+let b = "kashif"
+console.log(b.toUpperCase)
+console.log(b.toLowerCase)
+console.log(b.length)
+//slice the with indexing
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+
+
+console.log(b.replace("ka" , "wa"))
+
+console.log(b.concat(a , "faiz" ,"Shadab"))
